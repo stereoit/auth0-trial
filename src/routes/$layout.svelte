c08e5fb2bcbf5fd4e458b8841c294ea19eea41b7
Robert <robert.smol@stereoit.com>
2020-11-10 22:12:26 +0100

    Initial commit

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Nav from '$components/nav.svelte';
+</script>
+
+<main>
+	<Nav />
+	<slot></slot>
+</main>
+
+<style>
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+
+</style>
